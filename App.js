49db4dce88a5b0e2a67eaaee78e9988c4a224956
Adam Xiao <42500628+adam-xiao@users.noreply.github.com>
2020-09-02 03:52:58 -0400

    Setting up the homescreen

--- App.js
+++ App.js
@@ -1,21 +1,92 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends Component {
+
+  state={
+    currentUser: { username: "", id: null },
+    media: [],
+    authMode: true
+  }
+
+
+  componentDidMount(){
+    const token = localStorage.token
+
+    if (token) {
+      //get user info if token is present 
+      fetch("http://localhost:3000/auto_login", {
+        headers: {
+          "Authorization": token
+        }
+      })
+        .then(res => res.json())
+        .then(response => {
+          if (response.errors) {
+            alert(response.errors)
+          } else {
+            this.setState({
+              currentUser: response
+            })
+          }
+        })
+      
+      fetch(`http://localhost:3000/getmedia`, {
+        headers: {
+          "Authorization": localStorage.token
+        }
+      })
+        .then(resp => resp.json())
+        .then(data => { 
+          this.setState({ media: data })
+          console.log(data)
+        }
+      )
+
+    }
+  }
+
+  render(){
+
+    const styles = StyleSheet.create({
+      container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+    });
+
+    return (
+      <View style={styles.container}>
+        <Text>Daily Dues!</Text>
+        <TextInput
+          autoCapitalize="none"
+          placeholder="Username"
+          // onChangeText={}
+        />
+        <TextInput
+          secureTextEntry={true}
+          placeholder="Password"
+          // onChangeText={}
+        />
+        
+        
+        
+        <Button
+          title="Login"
+          onPress={() => Alert.alert('Simple Button pressed')}
+        />
+        <Button
+          title="Register"
+          onPress={() => Alert.alert('Simple Button pressed')}
+        />
+
+
+
+        <StatusBar style="auto" />
+      </View>
+    )
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
