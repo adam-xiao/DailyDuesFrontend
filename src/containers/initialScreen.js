8db607875e2e3e81f9fab990e9370ac9f37a0a38
Adam Xiao <42500628+adam-xiao@users.noreply.github.com>
2020-10-03 21:42:57 -0400

    Create initialScreen.js

---
+++ src/containers/initialScreen.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+
+export default class intialScreen extends Component {
+    
+    render(){
+        return(
+            null
+        )
+    }
+}
